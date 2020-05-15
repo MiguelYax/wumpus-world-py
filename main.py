9592a16a5b8f0c84471c7f13ee7a157d751d73fa
Miguel Yax <mig_dj@hotmail.com>
2020-05-15 06:54:41 -0600

    add basic exploration

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 #################### CONSTANT ####################
 
 WUMPUS_WORLD_DIMENSION = 3
+SEPARATOR = ','
 
 WUMPUS = 'W'
 PIT = 'p'
@@ -25,7 +26,7 @@ VISITED = 'v'
 
 #################### WUMPUS WORLD ####################
 
-world = [['', '', '', ''],
+world = [[AGENT, '', '', ''],
          ['', '', '', ''],
          ['', '', '', ''],
          ['', '', '', '']]
@@ -49,19 +50,34 @@ def is_wall(x, y):
 
 def add(x, y, value):
     if value != "" and is_wall(x, y) == False:
-        if world[x][y] == "":
+        if is_empty(x, y):
             world[x][y] = value
         else:
-            world[x][y] += "," + value
+            if find(x, y, value) == False:
+                world[x][y] += SEPARATOR + value
+
+
+def remove(x, y, value):
+    if is_wall(x, y) == False:
+        values = world[x][y]
+        world[x][y] = values.replace(SEPARATOR + value, '').replace(value, '')
 
 
 def find(x, y, value):
     currentValue = world[x][y]
-    values = currentValue.split(',')
+    values = currentValue.split(SEPARATOR)
     for i in range(len(values)):
         if values[i] == value:
             return True
     return False
+
+
+def is_empty(x, y):
+    if is_wall(x, y) == False:
+        if world[x][y] == "":
+            return True
+    else:
+        False
 
 
 def set_wumpus():
@@ -111,19 +127,143 @@ def set_gold():
             # set sensors
             add(x, y, GLITTER)
 
+
+def build_world():
+    set_wumpus()
+    set_pit()
+    set_pit()
+    set_pit()
+    set_gold()
+
+    print_world()
+
+
+def print_world():
+    print(world[0])
+    print(world[1])
+    print(world[2])
+    print(world[3])
+    print('')
+
+#################### AGENT METHODS ####################
+
+
+"""
+POSITION MAP
+
+[[0,0],[0,1],[0,2],[0,3],
+ [1,0],[1,1],[1,2],[1,3],
+ [2,0],[2,1],[2,2],[2,3],
+ [3,0],[3,1],[3,2],[3,3]]
+
+"""
+
+
+def get_information(x, y):
+
+    if is_wall(x, y):
+        return {
+            "bump": True
+        }
+    else:
+        return {
+            "bump": False,
+            "stench": find(x, y, STENCH),
+            "breeze": find(x, y, BREEZE),
+            "glitter": find(x, y, GLITTER),
+            "visited": find(x, y, VISITED),
+            "ok": find(x, y, OK)
+        }
+
+
+"""
+   │ F │
+───┼───┼───
+ L │ A │ R
+───┼───┼───
+   │ B │
+
+A: Agent position   (x,y)
+L: Left             (x-1,y)
+R: Right            (x-1,y)
+F: Forward          (x,y+1)
+B: Backward         (x,y-1)
+"""
+
+
+def next(x, y):
+    # check current position
+    if is_empty(x, y):
+        add(x, y, OK)
+
+    current = get_information(x, y)
+    left = get_information(x - 1, y)
+    right = get_information(x + 1, y)
+    forward = get_information(x, y+1)
+    backward = get_information(x, y-1)
+    # use all information to make a move.
+    if left.bump == False and forward.bump == False and right.bump == False and forward.bump == False:
+        print('All Information')
+
+    if left.bump == True:
+        if forward.bump == True:
+            # Agent is in a corner 
+            if backward.bump == False and right.bump == False:
+"""
+   │   │
+───┼───┼───
+   │ b │ b
+───┼───┼───
+   │ b │ 
+"""
+                # use backward and right to take a desition.
+                if current.breeze == True and 
+
+        # use left, forward and current information to make a move.
+        
+
+
+def check_stauts(x, y):
+    agent_die = False
+    found_gold = find(x, y, GOLD)
+
+    if find(x, y, WUMPUS) or find(x, y, PIT):
+        agent_die = True
+
+    return found_gold, agent_die
+
+
+def exploring_world():
+    current_x = 0
+    current_y = 0
+
+    limit = 0
+
+    while found_gold == False and agent_die == False and limit < 100:
+
+        current_x, current_y = next(current_x, current_y)
+
+        found_gold, agent_die = check_stauts(current_x, current_y)
+        # add(new_x, new_y, AGENT)
+        # remove(current_x, current_y, AGENT)
+        # add(current_x, current_y, VISITED)
+        print_world()
+        limit += 1
+
+    if found_gold:
+        print("Gold is found")
+
+    if agent_die:
+        print("agent is died")
+
+    if limit > 98:
+        print("Infinite loop")
+
 #################### INITIALIZE WUMPUS WORLD ####################
 
 
-set_wumpus()
-set_pit()
-set_pit()
-set_pit()
-set_gold()
-
-print(world[0])
-print(world[1])
-print(world[2])
-print(world[3])
+build_world()
 
 #################### EXPLORING THE WUMPUS WORLD ####################
 
+exploring_world()
